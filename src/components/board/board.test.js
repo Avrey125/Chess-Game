89ad31757229b5c022d367a5d692d7d90d2c2362
Avrey Brown <avrey.brown@globant.com>
2020-05-15 11:38:46 -0700

    wrote more tests

--- src/components/board/board.test.js
+++ src/components/board/board.test.js
@@ -30,17 +30,43 @@ describe('board.js', () => {
     let board = null;
     board = TestRenderer.create(<Board />);
     const boardAsJson = board.toJSON();
-    expect(boardAsJson.props.className).toEqual('wrapper');
-    console.log(boardAsJson);
+    expect(boardAsJson.props.className).toEqual('wrapper')
   });
   it('contains 64 square objects with their own individual values', () => {
     let board = null;
     board = TestRenderer.create(<Board />);
     const boardAsJson = board.toJSON();
-    expect(boardAsJson.children[0].props.className).toEqual('top');
-    expect(boardAsJson.children[1].props.className).toEqual('box');
-    expect(boardAsJson.children[1].props.value).toBe('A1');
-    expect(boardAsJson.children[65].props.className).toEqual('bottom');
-    expect(boardAsJson.children.length).toEqual(66);
+    const coordinateValues = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1','A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2','A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3','A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4','A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5','A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6','A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7','A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8']
+    // expect(boardAsJson.children[0].props.className).toEqual('top');
+    // expect(boardAsJson.children[1].props.className).toEqual('box');
+    // expect(boardAsJson.children[1].props.value).toBe('A1');
+    // expect(boardAsJson.children[65].props.className).toEqual('bottom');
+    // expect(boardAsJson.children.length).toEqual(66);
+    // splice the children from 1-64, creating an array of those children 
+    // map eachh one, and return a new array of just the values 
+    // does that = what I expect? 
+    // newAr r= spiced boardAsJson
+    let squares = boardAsJson.children.splice(1,64);
+    console.log(squares)
+
+    let coordinates = squares.map( (square) => {
+      return square.props.value;
+    });
+
+    expect(coordinates).toEqual(coordinateValues);
   })
-})
+  it('will not contain a coordinate value past 8', () => {
+    const expected = ['H9'];
+    const expected2 = ['A9'];
+
+    let board = null;
+    board = TestRenderer.create(<Board />);
+    const boardAsJson = board.toJSON();
+    expect(boardAsJson.children).toEqual(
+      expect.not.arrayContaining(expected),
+    );
+    expect(boardAsJson.children).toEqual(
+      expect.not.arrayContaining(expected2),
+    );
+  });
+});
