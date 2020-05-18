b2f7be86890e1cdbff2c1727d554877463a80393
Avrey Brown <avrey.brown@globant.com>
2020-05-18 08:59:43 -0700

    finished testing

--- src/components/board/board.test.js
+++ src/components/board/board.test.js
@@ -37,15 +37,6 @@ describe('board.js', () => {
     board = TestRenderer.create(<Board />);
     const boardAsJson = board.toJSON();
     const coordinateValues = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1','A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2','A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3','A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4','A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5','A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6','A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7','A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8']
-    // expect(boardAsJson.children[0].props.className).toEqual('top');
-    // expect(boardAsJson.children[1].props.className).toEqual('box');
-    // expect(boardAsJson.children[1].props.value).toBe('A1');
-    // expect(boardAsJson.children[65].props.className).toEqual('bottom');
-    // expect(boardAsJson.children.length).toEqual(66);
-    // splice the children from 1-64, creating an array of those children 
-    // map eachh one, and return a new array of just the values 
-    // does that = what I expect? 
-    // newAr r= spiced boardAsJson
     let squares = boardAsJson.children.splice(1,64);
     console.log(squares)
 
